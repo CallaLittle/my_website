dba065534f2f15462d3ad6f664f48291841943c1
CallaLittle <Student@SHC-380-016.shc.wisc.edu>
2016-01-27 17:08:32 -0600

    Module 2 activity.
    First portion of module 2 activity

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 /* Calla Little, 2016 */
+
+//create list of objects to hold city data
+var cityData = [
+	{
+		name: 'Telluride',
+		pop: 2319
+	},
+
+	{
+		name: 'Campton',
+		pop: 429
+	},
+
+	{
+		name: 'Bessemer',
+		pop: 1835
+	},
+
+	{
+		name: 'Minneapolis'
+		pop: 400070
+	}
+];
+
+//create a table
+$('#mydiv').append('<table id=\'cityTable\'>');
+
+//add column headers
+$('#cityTable').append('<tr><th>City<th>Population');
